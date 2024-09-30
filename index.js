e9e9017d691b702103e7ad8cab2657748f5bf4c7
Your Name <usamax089@gmail.com>
2024-09-30 15:45:16 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(arrayofDrivers, string) {
+    let filterDrivers = arrayofDrivers.filter(driver => driver.toLowerCase() == string.toLowerCase())//toLowerCase to convert all strings nad make case insesnitive
+    return filterDrivers
+}
+
+//2.
+   function fuzzyMatch(driversArray, string) {
+        //need to query array return drivers provided throught the string 
+        return driversArray.filter(function(driver) {
+            if(driver[0] === string[0]) {
+                return string
+            }
+        })
+    }
+
+
+//3.should return each element whose name property matches the provided string argument 
+function matchName(arrOfDrivers, string) {
+    let nameMatches  = arrOfDrivers.filter(drivers => drivers.name === string )
+    return nameMatches
+}
